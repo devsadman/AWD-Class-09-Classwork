38906f2df798bf84b66b9b3e0d1bbc049ec1e497
sadman <ahsan.susmoy@gmail.com>
2021-09-11 10:49:00 +0600

    Git coomand file added

---
+++ git command.js
@@ -0,0 +1,21 @@
+
+	// Git Init & config
+		- git status 
+		- git init 
+		- git config --global user.name ""
+		- git config --global user.email ""
+		- git config --list 
+
+	// Add & Commit 
+		- git add .
+		- git commit -m "message of commit"
+		- git log 
+		- git log --oneline 
+		- git checkout serial 
+		- git switch - 
+
+	// Git Ignore 
+		- 
+
+
+
